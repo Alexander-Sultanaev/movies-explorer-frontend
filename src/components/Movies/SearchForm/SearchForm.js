23bbd9f534178cf0b67b6650f19160a2dd53cb8a
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2023-04-14 09:05:25 +0500

    Добавлен результат последнего поиска

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -3,21 +3,31 @@ import { useLocation } from "react-router-dom";
 import './SearchForm.css';
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 function SearchForm({ handleSearch, onFilter, shortMovies }) {
-  const[ inputValue, setInputValue ] = useState('')
+  const [ inputValue, setInputValue ] = useState('')
+  let textSearchMovies = 'Результат последнего поиска: ' + localStorage.getItem('textSearchMovies')
+  if (textSearchMovies === `Результат последнего поиска: null`) {
+    textSearchMovies = ('')
+  }
+  let textSearchSavedMovies = 'Результат последнего поиска: ' + localStorage.getItem('textSearchSavedMovies')
+  if (textSearchSavedMovies === `Результат последнего поиска: null`) {
+    textSearchSavedMovies = ('')
+  }
   const location = useLocation()
-  
+
   function handleInputValue (e) {
     setInputValue(e.target.value)
   }
-  
+
   function handleSubmitMovies (e) {
     e.preventDefault()
     handleSearch(inputValue, shortMovies)
+    localStorage.setItem('textSearchMovies', inputValue)
   }
 
   function handleSaveSavedMovie (e) {
     e.preventDefault()
     handleSearch(inputValue, shortMovies)
+    localStorage.setItem('textSearchSavedMovies', inputValue)
   }
   
   return(
@@ -33,6 +43,7 @@ function SearchForm({ handleSearch, onFilter, shortMovies }) {
               onChange={handleInputValue}
               required
             />
+            <span className="search-form__span">{textSearchMovies}</span>
             <button className="search-form__button" type="submit">Найти</button>
           </form>
         : <></>}
@@ -45,12 +56,14 @@ function SearchForm({ handleSearch, onFilter, shortMovies }) {
               onChange={handleInputValue}
               required
             />
+            <span className="search-form__span">{textSearchSavedMovies}</span>
             <button className="search-form__button" type="submit">Найти</button>
           </form>
         : <></>
         )}
         <FilterCheckbox onFilter={onFilter} isMovieFilter={shortMovies}/>
       </div>
+      
     </section>
   );
 };
