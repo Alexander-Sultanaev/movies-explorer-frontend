609410f6de5a330cbb2a262ba829d76161ca7e49
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2023-04-25 15:29:05 +0500

    Добавлены константы для чисел

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
-//const BASE_URL = 'https://api.sultanaev.diplom.nomoredomains.rocks';
+const BASE_URL = 'https://api.sultanaev.diplom.nomoredomains.rocks';
 
 class MainApi {
   constructor(options) {
